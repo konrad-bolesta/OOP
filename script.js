b0c3dd609d4638ea03f61cadda49d862b7979836
Konrad <konrad.bolesta@gmail.com>
2016-11-29 23:15:56 +0100

    v2

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ var iPhone6S = new Telefon("Apple", 2250, "srebrny", 2015);
 var OnePlusOne = new Telefon("OnePlus", 750, "biały", 2014);
 var XPeriaXZ = new Telefon("Sony", 2500, "kremowy", 2016)
 
-SamsungGalaxyS6.printInfo();
-iPhone6S.printInfo();
-OnePlusOne.printInfo();
-XPeriaXZ.printInfo();
-
 function Telefon(marka, cena, kolor, rok) {
     this.marka = marka;
     this.cena = cena;
@@ -15,6 +10,16 @@ function Telefon(marka, cena, kolor, rok) {
     this.rok = rok;
 }
 
-Telefon.prototype.printInfo = function() {
-    console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", cena to " + this.cena + ", a rok produkcji to " + this.rok + ".");
+Telefon.prototype.getWarrantyCost = function () {
+	console.log("Możesz także przydłużyć gwarancję, będzie to kosztować " + this.cena / 10 + " zł.");
 }
+
+Telefon.prototype.printInfo = function() {
+    console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", cena to " + this.cena + " zł, a rok produkcji to " + this.rok + ".");
+    this.getWarrantyCost();
+}
+
+SamsungGalaxyS6.printInfo();
+iPhone6S.printInfo();
+OnePlusOne.printInfo();
+XPeriaXZ.printInfo();
